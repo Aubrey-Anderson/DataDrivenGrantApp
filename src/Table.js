b5b7e444cde429c50e519b96e7ccc7352b59b4c9
Aubrey Anderson <aubrey.anderson444@gmail.com>
2026-01-16 12:30:42 -0500

    Got the Institution and the Project Titles into rows displayed on my table

---
+++ src/Table.js
@@ -0,0 +1,12 @@
+import TableRow from "./TableRow.js"
+function Table(props){
+    return(
+        <table>
+            <tbody>
+                {props.grants.map(grant => <TableRow key={grant["@AppNumber"]} grant={grant} />)}
+            </tbody>
+        </table>
+    );
+}
+
+export default Table;
